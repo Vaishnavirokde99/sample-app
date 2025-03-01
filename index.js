5d659f16cccba3c49f48035995eae114e53b6b73
Vaishnavi <77009857+Vaishnavirokde99@users.noreply.github.com>
2025-03-01 11:29:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.post("/items", async (req, res) => {
   res.json(newItem);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
